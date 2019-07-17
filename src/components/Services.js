bfa955b2ab72108106772d0f0262be4f4c2b54f2
Marcus Zierke <marcuszierke@gmail.com>
2019-07-17 19:06:57 +0200

    animation in hero added

--- src/components/Services.js
+++ src/components/Services.js
@@ -14,31 +14,37 @@ class Services extends Component {
         <div className="row">
           <div className="services-headline">What We Do</div>
           <div className="services-sub-headline">
-            We believe in the lean startup principle that everything we build
-            should be of value for the user. Therefore our work does not start
-            with the question ’Can we build it?’ but rather with ‘Should we
-            build it?’. We want the best possible product for you and your
-            users.
+            We believe in the lean startup principle that everything we
+            build should be of value for the user. Therefore our work does
+            not start with the question ’Can we build it?’ but rather with
+            ‘Should we build it?’. We want the best possible product for you
+            and your users.
             <br />
             <br />
             <div style={{ fontWeight: "bold" }}>
               This is why we are more than IT.
             </div>
           </div>
-          <div className="services-boxes">
-            <div className="services-box peach">
-              <div className="services-box-headline">Consulting</div>
+          <div className="services-boxes-wrapper">
+            <div className="services-boxes">
+              <div className="services-box frame peach">
+                <div className="services-box-details">
+                  <div className="services-box-headline">Consulting</div>
+                  <p>Agile Methods</p>
+                  <p>Design Thinking</p>
+                </div>
+              </div>
+              <div className="services-box darkGrey">
+                <div className="services-box-headline">Concept</div>
+              </div>
             </div>
-            <div className="services-box darkGrey">
-              <div className="services-box-headline">Concept</div>
-            </div>
-          </div>
-          <div className="services-boxes">
-            <div className="services-box lightGrey">
-              <div className="services-box-headline">Development</div>
-            </div>
-            <div className="services-box pantone">
-              <div className="services-box-headline">Tech Stack</div>
+            <div className="services-boxes">
+              <div className="services-box lightGrey">
+                <div className="services-box-headline">Development</div>
+              </div>
+              <div className="services-box pantone">
+                <div className="services-box-headline">Tech Stack</div>
+              </div>
             </div>
           </div>
         </div>
