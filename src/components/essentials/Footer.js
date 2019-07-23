465f76f071a8b92a1e2c11cc4de929a267fe4dfc
Marcus Zierke <marcuszierke@gmail.com>
2019-07-23 16:11:14 +0200

    footer fixed

--- src/components/essentials/Footer.js
+++ src/components/essentials/Footer.js
@@ -4,11 +4,9 @@ import '../../styles/Footer.scss';
 const Footer = () => {
   return (
     <div className="footer-wrapper">
-      <div className="row">
-        <div className="footer-container">
-          <p><strong>Codein </strong> - Your destination for impactful digital products.</p>
-          <a href="https://s3.eu-central-1.amazonaws.com/adrilina/imprint.html" target="_blank">Impressum</a>
-        </div>
+      <div className="row footer-container">
+        <p><strong>Codein </strong> - Your destination for impactful digital products.</p>
+        <a href="https://s3.eu-central-1.amazonaws.com/adrilina/imprint.html" target="_blank">Impressum</a>
       </div>
     </div>
   );
