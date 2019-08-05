08dca46dab766d0033fa8231fa63f58dc71e7221
Jannis Köckritz <jannis.koeckritz@gmx.de>
2019-08-05 09:49:51 +0200

    Add translations

--- src/components/essentials/Footer.js
+++ src/components/essentials/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../styles/Footer.scss';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <div className="footer-wrapper">
       <div className="row footer-container">
-        <p><strong>Codein </strong> - Your destination for impactful tech products.</p>
+        <p><strong>Codein </strong> - {t('footer')}</p>
         <a href="https://s3.eu-central-1.amazonaws.com/adrilina/imprint.html" target="_blank">Impressum</a>
       </div>
     </div>
